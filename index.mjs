e000397202bc5fe965da3e34698514fd7f01d7e1
Kipngetich33 <khalifngeno@gmail.com>
2022-08-27 00:11:21 +0300

    add first successful vote counting with parallell reduce

--- index.mjs
+++ index.mjs
@@ -1,27 +1,94 @@
+// import all the third party libraries here
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
+import { ask, yesno, done } from '@reach-sh/stdlib/ask.mjs'
+
+// load the stdlib environment variables
 const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+// define contestants 
+const contestants = {
+  "President":[
+    {'fullName':"Raila Oding"},
+    {'fullName':"William Ruto"}
+  ],
+  "Gorvernor":[
+    {'fullName':"John Sakaja"},
+    {'fullName':"Polycarp Igathe"}
+  ]
+}
 
-console.log('Launching...');
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+console.log("********************************************* Voting System ***************************************************")
+console.log("Starting Contract")
+// create an account for the vote cordinator
+const accVoteCordinator  =  await stdlib.newTestAccount(startingBalance);
+console.log('Created account for accVoteCordinator');
 
-console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
-]);
 
-console.log('Goodbye, Alice and Bob!');
+// define helper functions
+const getContestantsID = async () => {
+  console.log("Getting Contestant ID")
+  const voterId = await ask(
+    'Enter your Id No.',
+    (x => x) // use call back to return the details entered by the user
+  )
+  return {
+    contestantId: voterId
+  }
+}
+
+let votingDone = false;
+const voters = []
+const startVotters = async () => {
+  console.log("starting Voters")
+
+  const runVotter = async (passedVote) => {
+    console.log("Running Voter")
+     // voters.push(vote);
+    const acc = await stdlib.newTestAccount(startingBalance);
+    console.log(`Create account for Voter ${passedVote}}`)
+
+    // define contract for the current user
+    const ctc = acc.contract(backend, ctcVoteCordinator.getInfo());
+    // call the voter api
+    try{
+      // now increment the vote here
+      const [ currentVote ] = await ctc.apis.Voter.vote(passedVote);
+      console.log(`Your latest vote is ${currentVote}`)
+    }catch (e) {
+      console.log(`Failed to Vote`)
+    } 
+  }
+
+  // now run voters votes here
+  await runVotter(1)
+  await runVotter(1)
+  await runVotter(1)
+
+  while(! votingDone ) {
+    await stdlib.wait(1);
+  }
+}
+
+
+// allow accVoteCordinator to attach to the backend as the deployer of the contract
+const ctcVoteCordinator = accVoteCordinator.contract(backend);
+// crate a partcipant interface for the voter cordininator
+await ctcVoteCordinator.participants.VoteCordinator({
+  votingReady: () => {
+    startVotters();
+  },
+  voteStatus: (vote) => {
+    console.log(`User Vote ${vote}`);
+  },
+  finalVote: (vote) => {
+    console.log(`Final Total Vote: ${vote}`);
+  },
+})
+
+console.log('Thanks for Making you voice Heard');
+// exit from active contract
+votingDone = true
+// done()
--- index.rsh
+++ index.rsh
@@ -3,21 +3,89 @@
 export const main = Reach.App(() => {
   // define the vote Cordinator interface
   const VoteCordinator = Participant('VoteCordinator', {
-    
+    getContestantsID: Fun([],Object({
+      contestantId:UInt,
+    })),
+    votingReady: Fun([],Null),
+    voteStatus: Fun([UInt],Null),
+    finalVote: Fun([UInt],Null),
   });
+
+
+  // define contestants
+  const contestants = {
+    "President":[
+      {'fullName':"Raila Oding"},
+      {'fullName':"William Ruto"}
+    ],
+    "Gorvernor":[
+      {'fullName':"John Sakaja"},
+      {'fullName':"Polycarp Igathe"}
+    ]
+  }
 
   // define the voter API interface
   const Voter = API('Voter', {
-
+    vote: Fun([UInt],Tuple(UInt)),
   });
 
   init();
-  // // The first one to publish deploys the contract
-  // A.publish();
-  // commit();
-  // // The second one to publish always attaches
-  // B.publish();
-  // commit();
-  // // write your program here
+
+  // now get the contest ID (VoteCordinator local step)
+  VoteCordinator.only(() => {
+    // const enteredContestantId = declassify(interact.getContestantsID())
+    // const getContestastants = getContestants
+  })
+
+  // The VoteCordinator is the first one to publish and hence pay deployment fees
+  VoteCordinator.publish();
+  commit();
+
+
+  VoteCordinator.publish();
+  const lenInBlocks = 50
+  const end = lastConsensusTime() + lenInBlocks;
+  const firstVote = 0
+
+  VoteCordinator.interact.votingReady();
+
+  const [ currentVote, ] = parallelReduce([ firstVote, ])
+    .invariant(balance() >= 0 )
+    .while(lastConsensusTime() <= end )
+    .api_(Voter.vote, (vote) => {
+      // add dynamic assertions
+      // check(bid > lastPrice, "Bid is too low.");
+
+      // now add the new amounts
+      // currentVote
+      // currentVote = currentVote + firstVote
+      // return calcuates amount
+      return [vote, (notify) => {
+        notify([vote]);
+
+        // if(! isFirstBid){
+        //   transfer(lastPrice).to(highestBidder)
+        // }
+        // const who = this;
+        VoteCordinator.interact.voteStatus(vote);
+        return [currentVote + vote]
+      }];
+    })
+    .timeout(absoluteTime(end), () => {
+      VoteCordinator.publish();
+      return [currentVote]
+    });
+  
+
+  // print the last vote here
+  VoteCordinator.interact.finalVote(currentVote)
+
+
+  transfer(balance()).to(VoteCordinator);
+
+  commit();
+
+  // end contract
   // exit();
+
 });
